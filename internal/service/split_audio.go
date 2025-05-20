98d8200afbe78b214f83db94557db5605df45a29
PairZhu <1115306638@qq.com>
2025-05-20 14:23:39 +0800

    feat: implement auto split algorithm

--- internal/service/split_audio.go
+++ internal/service/split_audio.go
@@ -10,13 +10,15 @@ import (
 )
 
 const (
-	SAMPLE_RATE     = 3000
-	DURATION_SECOND = 2 // 计算音频能量的时间长度
+	SAMPLE_RATE            = 3000
+	ENERGY_WINDOW_DURATION = 0.5  // 计算音频能量的时间长度
+	ERROR_RATE             = 0.1  // 误差率
+	MIN_DURATION           = 0.25 // 最小音频时长比
 )
 
-func buildFFmpegCmd(input string, start, end float32) (*exec.Cmd, error) {
+func buildFFmpegCmd(input string, start, end float64) (*exec.Cmd, error) {
 	if start < 0 || end <= start {
-		return nil, fmt.Errorf("invalid start or end time")
+		return nil, fmt.Errorf("invalid start or end time: start=%f, end=%f", start, end)
 	}
 	cmd := exec.Command(
 		storage.FfmpegPath,
@@ -34,7 +36,7 @@ func buildFFmpegCmd(input string, start, end float32) (*exec.Cmd, error) {
 	return cmd, nil
 }
 
-func getSilenceTimePoint(input string, start, end float32) (second float32, err error) {
+func getQuietestTimePoint(input string, start, end float64) (second float64, err error) {
 	cmd, err := buildFFmpegCmd(input, start, end)
 	if err != nil {
 		return 0, fmt.Errorf("failed to build ffmpeg command: %w", err)
@@ -50,7 +52,7 @@ func getSilenceTimePoint(input string, start, end float32) (second float32, err 
 
 	originBuffer := make([]byte, 1024)
 	headBuffer := [2]byte{}
-	circularQueue := util.NewCircularQueue[float32](SAMPLE_RATE * DURATION_SECOND)
+	circularQueue := util.NewCircularQueue[float32](SAMPLE_RATE * ENERGY_WINDOW_DURATION)
 	currentEnergy := float32(0)
 	index := 0
 	var (
@@ -86,12 +88,63 @@ func getSilenceTimePoint(input string, start, end float32) (second float32, err 
 
 			if currentEnergy <= minEnergy {
 				minEnergy = currentEnergy
-				minEnergyIndex = index - SAMPLE_RATE*DURATION_SECOND/2
+				minEnergyIndex = index - SAMPLE_RATE*ENERGY_WINDOW_DURATION/2
 			}
 		}
 	}
 	if err := cmd.Wait(); err != nil {
 		return 0, fmt.Errorf("ffmpeg command failed: %w", err)
 	}
-	return float32(minEnergyIndex)/SAMPLE_RATE + start, nil
+	return float64(minEnergyIndex)/SAMPLE_RATE + start, nil
+}
+
+func IterateSplitTimePoints(input string, segmentDuration float64) func(yield func(float64, float64) bool) error {
+	return func(yield func(float64, float64) bool) error {
+		audioDuration, err := util.GetAudioDuration(input)
+		if err != nil {
+			return fmt.Errorf("failed to get audio duration: %w", err)
+		}
+
+		beginTime := 0.0
+		for audioDuration-beginTime > segmentDuration {
+			timePoint, err := getQuietestTimePoint(
+				input,
+				beginTime+(1-ERROR_RATE)*segmentDuration,
+				beginTime+(1+ERROR_RATE)*segmentDuration,
+			)
+			if err != nil {
+				return fmt.Errorf("failed to get silence time point: %w", err)
+			}
+
+			if audioDuration-timePoint < segmentDuration*MIN_DURATION {
+				break
+			}
+
+			if !yield(beginTime, timePoint) {
+				return nil
+			}
+			beginTime = timePoint
+		}
+		yield(beginTime, audioDuration)
+		return nil
+	}
+}
+
+func ClipAudio(input, output string, start, end float64) error {
+	if start < 0 || end <= start {
+		return fmt.Errorf("invalid start or end time: start=%f, end=%f", start, end)
+	}
+	cmd := exec.Command(
+		storage.FfmpegPath,
+		"-y",
+		"-ss", fmt.Sprintf("%.3f", start), // 起始时间
+		"-to", fmt.Sprintf("%.3f", end), // 结束时间
+		"-i", input,
+		"-c:a", "copy",
+		output,
+	)
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to clip audio: %w", err)
+	}
+	return nil
 }
