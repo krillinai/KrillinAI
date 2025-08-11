7b514bf46ddf8f25850c2587c4062c3a8f2ff2e6
puji4810 <2430438586@qq.com>
2025-08-12 00:15:05 +0800

    更新YouTube字幕测试

--- internal/service/youtube_subtitle_test.go
+++ internal/service/youtube_subtitle_test.go
@@ -2,13 +2,13 @@ package service
 
 import (
 	"context"
+	"krillin-ai/config"
 	"krillin-ai/internal/types"
 	"testing"
 )
 
 func Test_YoutubeSubtitle(t *testing.T) {
 	// 固定的测试文件路径
-	subtitleFile := "/home/puji/KrillinAI/tasks/watch_vfLoHojLh_685Z/converted_subtitle.srt"
 	s := initService()
 
 	// 创建一个测试用的 SubtitleTask
@@ -19,15 +19,24 @@ func Test_YoutubeSubtitle(t *testing.T) {
 		Status:         1, // 处理中
 		ProcessPct:     0,
 	}
+	config.Conf.App.MaxSentenceLength = 100
+
+	_, err := s.YouTubeSubtitleSrv.convertToSrtFormat("/home/puji/KrillinAI/tasks/watch_vmdgIpmoF_dkxs/test.vtt", "/home/puji/KrillinAI/tasks/watch_vmdgIpmoF_dkxs/")
+	if err != nil {
+		t.Errorf("convertToSrtFormat() error = %v", err)
+		return
+	}
+	subtitleFile := "/home/puji/KrillinAI/tasks/watch_vmdgIpmoF_dkxs//converted_subtitle.srt"
+
 
 	// 执行测试
-	err := s.YouTubeSubtitleSrv.TranslateSrtFile(context.Background(), &types.SubtitleTaskStepParam{
+	err = s.YouTubeSubtitleSrv.TranslateSrtFile(context.Background(), &types.SubtitleTaskStepParam{
 		TaskId:                   "kgysZPHh",
 		TaskPtr:                  testTask, // 提供有效的 TaskPtr
 		OriginLanguage:           "en",
 		TargetLanguage:           "zh_cn",
-		TaskBasePath:             "/home/puji/KrillinAI/tasks/watch_vfLoHojLh_685Z/",
-		OriginalSubtitleFilePath: "/home/puji/KrillinAI/tasks/watch_vfLoHojLh_685Z/test.vtt",
+		TaskBasePath:             "/home/puji/KrillinAI/tasks/watch_vmdgIpmoF_dkxs/",
+		OriginalSubtitleFilePath: "/home/puji/KrillinAI/tasks/watch_vmdgIpmoF_dkxs/test.vtt",
 	}, subtitleFile)
 	if err != nil {
 		t.Errorf("TranslateSrtFile() error = %v, want nil", err)
