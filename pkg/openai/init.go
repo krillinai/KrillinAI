e7cfcb7e76406fb0ccebee3b30c7e3035b6fe5b4
wulien <wulien88@gmail.com>
2025-11-05 11:45:07 +0800

    去掉llm的代理

--- pkg/openai/init.go
+++ pkg/openai/init.go
@@ -2,8 +2,6 @@ package openai
 
 import (
 	"github.com/sashabaranov/go-openai"
-	"krillin-ai/config"
-	"net/http"
 )
 
 type Client struct {
@@ -14,15 +12,6 @@ func NewClient(baseUrl, apiKey, proxyAddr string) *Client {
 	cfg := openai.DefaultConfig(apiKey)
 	if baseUrl != "" {
 		cfg.BaseURL = baseUrl
-	}
-
-	if proxyAddr != "" {
-		transport := &http.Transport{
-			Proxy: http.ProxyURL(config.Conf.App.ParsedProxy),
-		}
-		cfg.HTTPClient = &http.Client{
-			Transport: transport,
-		}
 	}
 
 	client := openai.NewClientWithConfig(cfg)
